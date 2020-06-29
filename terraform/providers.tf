9323dbc7daf15e65fc0e26251c7759ab656fdf13
Gérard Farand <gerard.farand@gfi.world>
2020-06-29 15:08:03 +0200

    corrected some syntax

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 ## Lease set up your credentials as env vars, so you don't have to hardcode them
 
 provider "aws" {
-  access_key = var.aws_access_key
+  secret_key = var.aws_access_key
   # secret_key = var.aws_secret_key  
   version = "~> 2.0"
   region  = var.aws_default_region
