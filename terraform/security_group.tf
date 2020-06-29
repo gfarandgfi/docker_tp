a8e0cd164d8f0bdae180f3639cca8f23380cae11
Gérard Farand <gerard.farand@gfi.world>
2020-06-29 14:55:10 +0200

    corrected some syntax

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,7 +1,7 @@
 # Opening port 22 so the machine is accessible via ssh
 resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
-  descrition = "allow ssh in and all out"
+  description = "allow ssh in and all out"
   vpc_id      = aws_vpc.formation_docker.id
 
   ingress {
@@ -18,5 +18,5 @@ resource "aws_security_group" "allow_ssh" {
       cidr_blocks     = ["0.0.0.0/0"]
     }
 
-  depends_on = [aws_vpc.formation_docke]
+  depends_on = [aws_vpc.formation_docker]
   }
