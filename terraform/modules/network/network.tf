483f36ab7c766823b3fff0d6f6142e352462be4f
Gérard Farand <gerard.farand@gfi.world>
2020-07-01 18:17:44 +0200

    minor syntax

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -11,8 +11,8 @@ resource "aws_subnet" "formation_docker" {
   vpc_id            = aws_vpc.formation_docker.id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.aws_default_zone
-  tags = var.subnet_tags
-  
+  tags = var.network_tags
+
   depends_on = [aws_vpc.formation_docker]
 }
 
