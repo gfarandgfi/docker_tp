e439959858c0b3654c2a61d7971d7fe8e7d5c4ce
Gérard Farand <gerard.farand@gfi.world>
2020-07-01 15:40:17 +0200

    minor syntax

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,60 +1,60 @@
 output "student_instance_1_public_ip" {
-  value = module.student_instance_1.public_ip
+  value = module.instances.student_instance_1_public_ip
 }
 
 output "student_instance_1_ssh_key_public" {
-  value = module.student_instance_1.public_key_openssh
+  value = module.instances.student_instance_1_ssh_key_public
 }
 
 output "student_instance_1_root_password" {
-  value = module.student_instance_1.admin_pass.result
+  value = module.instances.student_instance_1_root_password
 }
 
 output "student_instance_2_public_ip" {
-  value = module.student_instance_2.public_ip
+  value = module.instances.student_instance_2_public_ip
 }
 
 output "student_instance_2_ssh_key_public" {
-  value = module.student_instance_2.public_key_openssh
+  value = module.instances.student_instance_2_ssh_key_public
 }
 
 output "student_instance_2_root_password" {
-  value = module.student_instance_2.admin_pass.result
+  value = module.instances.student_instance_2_root_password
 }
 
 output "student_instance_3_public_ip" {
-  value = module.student_instance_3.public_ip
+  value = module.instances.student_instance_3_public_ip
 }
 
 output "student_instance_3_ssh_key_public" {
-  value = module.student_instance_3.public_key_openssh
+  value = module.instances.student_instance_3_ssh_key_public
 }
 
 output "student_instance_3_root_password" {
-  value = module.student_instance_3.admin_pass.result
+  value = module.instances.student_instance_3_root_password
 }
 
 output "student_instance_4_public_ip" {
-  value = module.student_instance_4.public_ip
+  value = module.instances.student_instance_4_public_ip
 }
 
 output "student_instance_4_ssh_key_public" {
-  value = module.student_instance_4.public_key_openssh
+  value = module.instances.student_instance_4_ssh_key_public
 }
 
 output "student_instance_4_root_password" {
-  value = module.student_instance_4.admin_pass.result
+  value = module.instances.student_instance_4_root_password
 }
 
 output "student_instance_5_public_ip" {
-  value = module.student_instance_5.public_ip
+  value = module.instances.student_instance_5_public_ip
 }
 
 output "student_instance_5_ssh_key_public" {
-  value = module.student_instance_5.public_key_openssh
+  value = module.instances.student_instance_5_ssh_key_public
 }
 
 output "student_instance_5_root_password" {
-  value = module.student_instance_5.admin_pass.result
+  value = module.instances.student_instance_5_root_password
 }
 
