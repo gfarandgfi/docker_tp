345b584dc92c560c98458c0e3a51b76e86a492cc
Gérard Farand <gerard.farand@gfi.world>
2020-07-01 15:00:19 +0200

    minor syntax

--- terraform/common_modules.tf
+++ terraform/common_modules.tf
@@ -1,6 +1,6 @@
 # Create five separate instances with different tags
 module "student_instance_1" {
-  source = "../modules/instances/"
+  source = "./modules/instances/"
   tags = {
     student_name = "tariqanoual"
     reason = "training"
@@ -8,7 +8,7 @@ module "student_instance_1" {
 }
 
 module "student_instance_2" {
-  source = "../modules/instances/"
+  source = "./modules/instances/"
   tags = {
     student_name = "ludovicbels"
     reason = "training"
@@ -16,7 +16,7 @@ module "student_instance_2" {
 }
 
 module "student_instance_3" {
-  source = "../modules/instances/"
+  source = "./modules/instances/"
   tags = {
     student_name = "loickervarec"
     reason = "training"
@@ -24,7 +24,7 @@ module "student_instance_3" {
 }
 
 module "student_instance_4" {
-  source = "../modules/instances/"
+  source = "./modules/instances/"
   tags = {
     student_name = "florentcambier"
     reason = "training"
@@ -32,7 +32,7 @@ module "student_instance_4" {
 }
 
 module "student_instance_5" {
-  source = "../modules/instances/"
+  source = "./modules/instances/"
   tags = {
     student_name = "iliyanpavlov"
     reason = "training"
