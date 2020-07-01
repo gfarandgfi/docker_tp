1d7d004796e880354fcc8e6acce0994a8630f199
Gérard Farand <gerard.farand@gfi.world>
2020-07-01 15:26:39 +0200

    minor syntax

---
+++ terraform/modules/instances/outputs.tf
@@ -0,0 +1,11 @@
+output "public_ip" {
+  value = aws_instance.public_ip
+}
+
+output "student_instance_1_ssh_key_public" {
+  value = tls_private_key.public_key_openssh
+}
+
+output "student_instance_1_root_password" {
+  random_string.admin_pass.result
+}
