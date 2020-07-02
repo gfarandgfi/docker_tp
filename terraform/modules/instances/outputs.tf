032d95ee1f148f03d6677ac9e1bbe76c6c0698a8
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 13:18:21 +0200

    syntax

--- terraform/modules/instances/outputs.tf
+++ terraform/modules/instances/outputs.tf
@@ -2,7 +2,8 @@ output "all_public_ips" {
   value = {
     for student_names in aws_instance.student:
       instance.id => instance.private_ip
-  }}
+  }
+}
 
 output "all_public_dns" {
   value = aws_instance.student.public_dns
