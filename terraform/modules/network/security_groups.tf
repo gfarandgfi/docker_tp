34539f1b061c987beb9119112bc54aab83021037
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 17:52:16 +0200

    syntax

--- terraform/modules/network/security_groups.tf
+++ terraform/modules/network/security_groups.tf
@@ -8,21 +8,21 @@ resource "aws_security_group" "common" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.formation_docker.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.formation_docker.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
     ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.formation_docker.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
