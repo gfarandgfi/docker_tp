af613174e63ce719b0517cf24ea158d3983e8a19
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 15:53:49 +0200

    syntax

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -18,7 +18,7 @@ module "instances" {
   subnet_id = module.network.subnet_id
   aws_instance_type = var.aws_instance_type
   aws_instance_ami = var.aws_instance_ami
-  security_group_ids = [${module.network.security_group_id_ssh}, ${module.network.security_group_id_http}, ${module.network.security_group_id_tls}]
+  security_group_ids = [module.network.security_group_id_ssh, module.network.security_group_id_http, module.network.security_group_id_tls]
   student_names = var.student_names
   # depends_on = [module.network]
 }
