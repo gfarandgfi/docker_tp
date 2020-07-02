87b3825b62fbfaecff60792f489afd43b200c599
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 17:14:10 +0200

    syntax

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -12,7 +12,6 @@ module "network" {
 # Create an instance for each student
 module "instances" {
   source            = "./modules/instances"
-  subnet_id = module.network.subnet_id
   aws_instance_type = var.aws_instance_type
   aws_instance_ami = var.aws_instance_ami
   aws_default_zone = var.aws_default_zone
