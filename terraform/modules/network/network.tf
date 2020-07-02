21956bcb655c07bb3a3fa6620ea8475f6a8e21cf
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 18:36:31 +0200

    syntax

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -24,7 +24,7 @@ resource "aws_route_table" "r" {
   vpc_id = aws_vpc.formation_docker.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.vpc_cidr_block
     gateway_id = aws_internet_gateway.formation_docker.id
   }
 
