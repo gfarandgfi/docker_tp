0e845cf712afe75ac442a8c95c9ddf1baab93f16
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 14:45:34 +0200

    syntax

--- terraform/modules/instances/instance.tf
+++ terraform/modules/instances/instance.tf
@@ -1,13 +1,13 @@
-# Create a primary network interface
-resource "aws_network_interface" "primary" {
-  for_each        = var.student_names
-  tags            = {
-    Name = "${each.value}"
-  }
-  subnet_id       = var.subnet_id
-  description     = "primary network interface"
-  security_groups = var.security_group_id
-}
+# # Create a primary network interface
+# resource "aws_network_interface" "primary" {
+#   for_each        = var.student_names
+#   tags            = {
+#     Name = "${each.value}"
+#   }
+#   subnet_id       = var.subnet_id
+#   description     = "primary network interface"
+#   security_groups = var.security_group_id
+# }
 
 # Provision a Debian 10 Buster machine 
 resource "aws_instance" "student" {
@@ -20,12 +20,12 @@ resource "aws_instance" "student" {
   key_name              = "formation_docker"
   security_groups       = var.security_group_id
   associate_public_ip_address = true
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.primary[each.key].id
-    # False is the only valid value at the time of writing this code. Should be changed to true as soon as supported
-    delete_on_termination = false
-  }
+  # network_interface {
+  #   device_index         = 0
+  #   network_interface_id = aws_network_interface.primary[each.key].id
+  #   # False is the only valid value at the time of writing this code. Should be changed to true as soon as supported
+  #   delete_on_termination = false
+  # }
   # Install the git and docker binaries
   provisioner "remote-exec" {
     script = "./files/install_server.sh"
