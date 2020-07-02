31db05fab93c56327e23fda52b1dae840eb91e08
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 17:59:17 +0200

    syntax

--- terraform/modules/network/security_groups.tf
+++ terraform/modules/network/security_groups.tf
@@ -1,20 +1,20 @@
 # Open port 22 so the machine is accessible via ssh
 resource "aws_security_group" "common" {
   name        = "common"
-  description = "allow common ports in and all out"
+  description = "allow all traffic in and out"
   vpc_id      = aws_vpc.formation_docker.id
 
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   depends_on = [aws_vpc.formation_docker]
