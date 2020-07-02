a00ed51856301323d9816bb16225307c62480611
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 13:05:20 +0200

    syntax

--- terraform/modules/instances/outputs.tf
+++ terraform/modules/instances/outputs.tf
@@ -1,7 +1,7 @@
 output "all_public_ips" {
-  value = aws_instance.student[*].public_ip
+  value = aws_instance.student.public_ip
 }
 
 output "all_public_dns" {
-  value = aws_instance.student[*].public_dns
+  value = aws_instance.student.public_dns
 }
