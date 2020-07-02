09d8eac9bd319434e6840e16405feb182b878d0e
Gérard Farand <gerard.farand@gfi.world>
2020-07-02 17:54:55 +0200

    syntax

--- terraform/modules/network/security_groups.tf
+++ terraform/modules/network/security_groups.tf
@@ -5,22 +5,8 @@ resource "aws_security_group" "common" {
   vpc_id      = aws_vpc.formation_docker.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-    ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
